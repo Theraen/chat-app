df098ac705d24f80d2af07b4d4f928b0de2ab0b6
Theraen <barraud.quentin@gmail.com>
2021-02-20 02:30:28 +0100

    Update component Contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -12,10 +12,11 @@ function Contact() {
           <img className="avatar" src={avatar} alt={name} />
           <div>
               <h4 className="name">{name}</h4>
-              <p className="status-text">
-                  <span className="status"><i className="status-online"></i></span>
-                  {isOnline ? "En ligne" : "Hors ligne"}
-              </p>
+              <div className="status">
+                  <i className="status-online"></i>
+                  <p className="status-text">{isOnline ? "En ligne" : "Hors ligne"}</p>
+              </div>
+              
           </div>
     </div>
   );
